677108359543c8e392c5999ee9a128d317398e8e
devkoepeli <devrim_kopeli@hotmail.com>
2024-04-11 17:42:48 +0200

    build: add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"dabubble-dab15","appId":"1:208784063504:web:cf4c78264aeb5e9b515e95","storageBucket":"dabubble-dab15.appspot.com","apiKey":"AIzaSyATtpEh3uhX5_sZaiOitRpVTqlRtIa_2IE","authDomain":"dabubble-dab15.firebaseapp.com","messagingSenderId":"208784063504"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage()))]
 };
