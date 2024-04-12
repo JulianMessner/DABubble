ea70573f38f70bc7ddfbf8fe2b4764638ce9384b
JulianMessner <julianmessner.13@gmail.com>
2024-04-12 19:14:16 +0200

    header and navbar component added

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { NavbarVerticalComponent } from './navbar-vertical/navbar-vertical.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NavbarVerticalComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
