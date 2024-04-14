2c6a900be4a50ae693f6626ee36c788569808d1a
JulianMessner <julianmessner.13@gmail.com>
2024-04-14 15:25:23 +0200

    direct message style adjusted

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NavbarVerticalComponent } from './navbar-vertical/navbar-vertical.component';
 import { MainChatComponent } from './main-chat/main-chat.component';
+import { DirectMessageComponent } from './direct-message/direct-message.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavbarVerticalComponent, MainChatComponent],
+  imports: [RouterOutlet, HeaderComponent, NavbarVerticalComponent, MainChatComponent, DirectMessageComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
