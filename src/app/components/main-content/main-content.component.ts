c0abbe184ef5c887bd1f27e6e2a89df783cee24e
JulianMessner <julianmessner.13@gmail.com>
2024-04-14 15:45:10 +0200

    added thread chat component

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './header/header.component';
 import { NavbarVerticalComponent } from './navbar-vertical/navbar-vertical.component';
 import { MainChatComponent } from './main-chat/main-chat.component';
 import { DirectMessageComponent } from './direct-message/direct-message.component';
+import { ThreadChatComponent } from './thread-chat/thread-chat.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavbarVerticalComponent, MainChatComponent, DirectMessageComponent],
+  imports: [RouterOutlet, HeaderComponent, NavbarVerticalComponent, MainChatComponent, DirectMessageComponent, ThreadChatComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
