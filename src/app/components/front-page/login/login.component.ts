1d2ac329451911036d40fa9a3d65aed31e51f2b0
devkoepeli <devrim_kopeli@hotmail.com>
2024-04-15 18:37:02 +0200

    style: login input fields

--- src/app/components/front-page/login/login.component.ts
+++ src/app/components/front-page/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, LoginFormComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
