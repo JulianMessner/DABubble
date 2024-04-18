c2aeda4865e1a8770b5b58881daaf36ef7f7f6d4
devkoepeli <devrim_kopeli@hotmail.com>
2024-04-18 15:56:15 +0200

    feat: validate register form

--- src/app/components/front-page/register/register-form/register-form.component.ts
+++ src/app/components/front-page/register/register-form/register-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UserService } from '../../../../services/user.service';
+import { User } from '../../../../models/user.interface';
 
 @Component({
   selector: 'app-register-form',
@@ -12,6 +14,7 @@ import { Router } from '@angular/router';
 export class RegisterFormComponent {
 
   formBuilder = inject(FormBuilder);
+  userService = inject(UserService);
   
   checkboxChecked = false;
 
@@ -21,15 +24,22 @@ export class RegisterFormComponent {
 
   constructor(private router: Router) {}
 
+  isNameActive = false;
+  isEmailActive = false;
+  isPasswordActive = false;
+
   registerForm = this.formBuilder.group({
     name: ['', [
-      Validators.required
+      Validators.required,
+      Validators.pattern(/^[A-Za-zäöüßéèêëçñàâ]+ [A-Za-zäöüßéèêëçñàâ]+$/)
     ]],
     email: ['', [
       Validators.required,
+      Validators.pattern(/[^@]+@[^\.]+\..+/)
     ]],
     password: ['', [
       Validators.required,
+      Validators.pattern(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/)
     ]],
     checkbox: ['', [
       this.checkboxValidator
@@ -52,9 +62,16 @@ export class RegisterFormComponent {
     return this.registerForm.get('checkbox');
   }
 
-  saveUser() {
+  saveUser(): void {
     if (this.registerForm.valid) {
-      console.log("Form data: " + this.registerForm.value);
+      const registerFormObj = this.registerForm.value;
+
+      const user: User = {
+        name: registerFormObj.name!,
+        email: registerFormObj.email!,
+        password: registerFormObj.password!
+      }
+      this.userService.setUser(user)
       this.router.navigate(['/avatar']);
     }
   }
