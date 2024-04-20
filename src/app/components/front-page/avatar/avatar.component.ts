53b30385565ee07876315304b7a4cba5c22afa00
devkoepeli <devrim_kopeli@hotmail.com>
2024-04-20 05:31:36 +0200

    feat: add initial avatar selection page

--- src/app/components/front-page/avatar/avatar.component.ts
+++ src/app/components/front-page/avatar/avatar.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserService } from '../../../services/user.service';
+import { RouterLink } from '@angular/router';
+import { User } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-avatar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.scss'
 })
 export class AvatarComponent {
 
+  userService = inject(UserService);
+
+  defaultAvatar = 'assets/images/default_avatar.svg';
+  selectedAvatar = '';
+
+  user = this.userService.getUser();
+
+  avatars: string[] = [
+    'assets/images/avatar1.svg', 
+    'assets/images/avatar2.svg',
+    'assets/images/avatar3.svg',
+    'assets/images/avatar4.svg',
+    'assets/images/avatar5.svg',
+    'assets/images/avatar6.svg'
+  ];
+
+  selectAvatar(avatarPath: string) {
+    this.userService.setAvatar(avatarPath);
+    this.selectedAvatar = avatarPath;
+  }
+
+  addUser() {
+    // this.userService.getUser() add with firestoreService 
+  }
 }
