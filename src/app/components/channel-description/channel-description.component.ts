7c7c66e0d09cc4a2cc8f22e602a91c7be0867171
JulianMessner <julianmessner.13@gmail.com>
2024-05-01 15:24:16 +0200

    channel description component finished style for mobile and desktop

--- src/app/components/channel-description/channel-description.component.ts
+++ src/app/components/channel-description/channel-description.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { MainChatComponent } from '../main-content/main-chat/main-chat.component';
 
 @Component({
@@ -7,10 +7,20 @@ import { MainChatComponent } from '../main-content/main-chat/main-chat.component
   standalone: true,
   imports: [CommonModule, MainChatComponent],
   templateUrl: './channel-description.component.html',
-  styleUrl: './channel-description.component.scss',
+  styleUrls: ['./channel-description.component.scss', './desktop-channel-description.component.scss'],
 })
 export class ChannelDescriptionComponent {
   isHovered: boolean = false;
+  nameEditMode: boolean = false;
+  isNameEditHovered: boolean = false;
+  descriptionEditMode: boolean = false;
+  isDescriptionEditHovered: boolean = false;
+  name: string = 'Entwicklerteam';
+  description: string =
+    'Dieser Channel ist für alles rund um #dfsdf vorgesehen. Hier kannst du zusammen mit deinem Team Meetings abhalten, Dokumente teilen und Entscheidungen treffen.';
+
+  @ViewChild('nameText') nameTextElement!: ElementRef;
+  @ViewChild('descriptionText') descriptionTextElement!: ElementRef;
 
   constructor(private mainChatComponent: MainChatComponent) {}
 
@@ -20,5 +30,39 @@ export class ChannelDescriptionComponent {
 
   changeImgSrc(isHovering: boolean) {
     this.isHovered = isHovering;
+  }
+
+  toggleNameEditMode() {
+    this.nameEditMode = !this.nameEditMode;
+    if (this.nameEditMode && this.nameTextElement) {
+      this.nameTextElement.nativeElement.focus();
+    } else if (!this.nameEditMode && this.nameTextElement) {
+      this.name = this.nameTextElement.nativeElement.innerText;
+    }
+  }
+
+  toggleDescriptionEditMode() {
+    this.descriptionEditMode = !this.descriptionEditMode;
+    if (this.descriptionEditMode && this.descriptionTextElement) {
+      this.descriptionTextElement.nativeElement.focus();
+    } else if (!this.descriptionEditMode && this.descriptionTextElement) {
+      this.description = this.descriptionTextElement.nativeElement.innerText;
+    }
+  }
+
+  onMouseEnterEdit() {
+    this.isNameEditHovered = true;
+  }
+
+  onMouseLeaveEdit() {
+    this.isNameEditHovered = false;
+  }
+
+  onMouseEnterDescriptionEdit() {
+    this.isDescriptionEditHovered = true;
+  }
+
+  onMouseLeaveDescriptionEdit() {
+    this.isDescriptionEditHovered = false;
   }
 }
